868356fcd3db11e72d67920b0511cb973de4cd21
Atif Aziz <code@raboof.com>
2016-06-15 18:53:07 +0200

    Generate nupkgs in dist

--- pack.cmd
+++ pack.cmd
@@ -1,1 +1,6 @@
-@nuget pack -Symbols "%~dp0\src\FakeLinqPad.csproj" %*
+@echo off
+pushd "%~dp0"
+if not exist dist mkdir dist || goto :end
+nuget pack -Symbols -OutputDirectory "dist" src\FakeLinqPad.csproj %*
+:end
+popd
